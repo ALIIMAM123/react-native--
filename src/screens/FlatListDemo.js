d86466cff297cf390c6fa7acb6005a65ef5eac95
Md Ali imam <imam.ali1751@gmail.com>
2022-07-16 23:11:48 +0530

    flat list keys and vertical and horizontal scrolling

--- src/screens/FlatListDemo.js
+++ src/screens/FlatListDemo.js
@@ -3,22 +3,26 @@ import { View, Text, FlatList, StyleSheet } from "react-native";
 
 const FlatListDemo = () => {
 	const names = [
-		{
-			name: "Ali",
-		},
-		{
-			name: "jitendre",
-		},
-		{
-			name: "dipesh",
-		},
-		{
-			name: "suresh",
-		},
+		{ index: "1", name: "Ali" },
+		{ index: "2", name: "Jitendre" },
+		{ index: "3", name: "Dipesh" },
+		{ index: "4", name: "Suresh" },
+		{ index: "5", name: "Rahul" },
+		{ index: "6", name: "Amarandra" },
+		{ index: "6", name: "Bhupesh" },
+		{ index: "7", name: "Sumit" },
 	];
 
 	return (
 		<FlatList
+			style={styles.listStyle}
+			keyExtractor={(key) => {
+				return key.index;
+			}}
+			// horizontal
+			showsHorizontalScrollIndicator={false}
+			// numColumns={2}
+			// inverted
 			data={names}
 			renderItem={(element) => {
 				console.log(element.item.name);
@@ -29,9 +33,22 @@ const FlatListDemo = () => {
 };
 
 const styles = StyleSheet.create({
+	listStyle: {
+		backgroundColor: "rgba(61, 65, 251, 0.54)",
+		// height: "100vh",
+		textAlign: "center",
+		padding: 10,
+		margin: 20,
+	},
 	textStyle: {
 		fontSize: 30,
-		color: "red",
+		color: "rgba(6, 9, 220, 0.54)",
+		backgroundColor: "rgba(237, 237, 243, 0.54)",
+		padding: 10,
+		margin: 20,
+		fontFamily: "roboto",
+		textAlign: "center",
+		fontWeight: "500",
 	},
 });
 
